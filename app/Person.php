73c4123b5a5aa6c453072232470565609288cd65
teionhozonn <teionhozonn@gmail.com>
2020-11-08 20:57:30 +0900

    today

--- app/Person.php
+++ app/Person.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    $msg = 'show people message!';
+    $result = Person::get();
+
+    $data = [
+      'msg' => $msg,
+      'result' => $result,
+    ];
+
+    return view('hello.index', $data);
 }
