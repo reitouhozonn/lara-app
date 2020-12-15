b58a132779099cf6c3a9ec21e40068136a7cc66d
teionhozonn <teionhozonn@gmail.com>
2020-12-15 22:43:07 +0900

    1215

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,12 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\person;
+use App\Jobs\MyJob;
+use Storage;
+// use Illuminate\Support\Facades\Storage;
+// use ScheduleObj;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +32,14 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $count = Person::all()->count();
+        $id = rand(0, $count) + 1;
+        $obj = new ScheduleObj($id);
+        $schedule->call($obj);
+
+
+        // $schedule->exec('bash ./mycmd.sh');
+        // $schedule->command('queue:work --stop-when-empty');
     }
 
     /**
@@ -39,4 +53,23 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+}
+
+
+class ScheduleObj
+{
+  private $person;
+
+  function __construct($id)
+  {
+    $this->person = Person::find($id);
+  }
+
+  public function __invoke()
+  {
+    Storage::append('person_access_log.txt',
+      now().' '.$this->person->all_data);
+    MyJob::dispatch($this->person);
+    return 'true';
+  }
 }
---
+++ mycmd.sh
@@ -0,0 +1,2 @@
+echo "[$(date '+%Y/%m/%d%r')] This is Mycmd.sh!!" >> mycmd_log.txt
+# echo " This is Mycmd.sh!!" >> mycmd_log.txt
