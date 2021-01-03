c0855c376451a2ea9e2f3959a6ba8e2e8c5599a7
teionhozonn <teionhozonn@gmail.com>
2021-01-03 18:15:18 +0900

    0103

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use database\factories\PersonFactory;
+use App\Person;
 
 class ExampleTest extends TestCase
 {
@@ -22,11 +24,27 @@ class ExampleTest extends TestCase
         // $this->post('/hello')->assertOk();
         $this->get('/react')->assertOk();
         // $this->post('/react')->assertOk();
-        $this->get('/hello/1')->assertOk();
+        // $this->get('/hello/1')->assertOk();
         // $this->get('/hoge')->assertStatus(404);
         $this->get('/hello')->assertSeeText('Index');
         $this->get('/hello')->assertSee('<h1>');
         $this->get('/hello')->assertSeeInOrder([
           '<html', '<head', '<body', '<h1>']);
+    }
+
+    public function testDbTest()
+    {
+        for ($i=0; $i < 100; $i++) {
+          // code...
+          // factory(Person::class)->create();
+        $count = Person::get()->count();
+        $person = Person::find($i+1);
+        $data = $person->toArray();
+
+        $this->assertDatabaseHas('people', $data);
+
+        $person->delete();
+        $this->assertDatabaseMissing('people', $data);
+      }
     }
 }
